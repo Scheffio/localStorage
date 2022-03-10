32ffc01aa0583a54bf05a1569618d96662d8202c
scheff <assi1122334455@gmail.com>
2022-03-10 13:43:11 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ let cancel = document.querySelector(".cancel")
 let table = document.querySelector("table")
 let timeText = document.querySelector(".right p:nth-child(1)")
 let username = document.querySelector(".right p:nth-child(2)")
+
 let counter = 0
 
+let modal = document.querySelector(".modal")
 
 input.addEventListener('input', () => {
     if (input.value.length >= 1) {
@@ -17,26 +19,60 @@ input.addEventListener('input', () => {
 })
 
 start.addEventListener("click", () => {
-    let starttime = new Date.now()
-
+    timer()
     username.textContent = input.value
+    input.value = ''
     click.removeAttribute('disabled')
     click.addEventListener("click", () => {
         counter++
+        console.log(counter)
         if (counter > 10) {
-
+            counter = 0
             start.setAttribute("disabled", true)
             click.setAttribute("disabled", true)
-            let endtime = new Date.now()
+            showWin()
+            addRecord(username.textContent, timefloor(date.getSeconds()), date.getMilliseconds())
         }
     })
 
 })
 
+function addRecord(name, seconds, milliseconds) {
+    let mas = [{}]
+    mas.forEach((elem, index) => {
+        elem[index] = name
+    })
+    localStorage.setItem("records", JSON.stringify(mas))
+    input.focus()
+}
+
+function showWin() {
+    let wintime = document.querySelector('.content-modal > p')
+    wintime.innerHTML = `Время игры ${timefloor(date.getSeconds())}.${(date.getMilliseconds())}`
+    clearInterval(getTimer)
+    modal.style.display = "flex"
+    setTimeout(()=> {
+        date = new Date(0,0,0,0,0,0,0,0)
+        timeText.textContent = "00.00"
+        username.textContent = "name"
+        modal.style.display = "none"
+    }, 2000)
+}
 
 
+let date = new Date(0,0,0,0,0,0,0,0)
 
+function timer() {
+    getTimer = setInterval(() => {
+        date.setSeconds(date.getSeconds())
+        date.setMilliseconds(date.getMilliseconds() + 5)
+        timeText.textContent = `${timefloor(date.getSeconds())}.${date.getMilliseconds()}`
+    }, 1)
+}
 
+function timefloor(n) {
+    return (n < 10) ? ('0' + n) : ('' + n)
+}
 
 window.onload = () => {
     start.setAttribute("disabled", true)
